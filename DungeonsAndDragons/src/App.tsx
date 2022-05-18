1d32d32e32fcba9e48285588b0c1da9e295d169d
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-05-18 15:28:38 +1000

    Added navigation banner.

--- DungeonsAndDragons/src/App.tsx
+++ DungeonsAndDragons/src/App.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <header>
-        This is the Dungeons and Dragons application.
-      </header>
-    </div>
+      <div className="container-fluid p-3 bg-light">
+          <header>
+              <nav className="navbar navbar-expand-lg navbar-light">
+                  <a className="navbar-brand" href="#">Dungeons and Dragons</a>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul className="navbar-nav mr-auto">
+                          <li className="nav-item active">
+                              <a className="nav-link" href="#">Home</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Spells</a>
+                          </li>
+                      </ul>
+                  </div>
+              </nav>
+          </header>
+          <div className="container m-0">
+              Placeholder for page content.
+          </div>
+      </div>
   );
 }
 
